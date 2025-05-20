3ef2121c0efdb5f106df527ed08f5eee2ba7ae60
Vicheka <vicheka@gmail.com>
2025-05-20 21:22:09 +0700

    Update vite.config.js for GitHub Pages and switch to HashRouter

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,22 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:8000',
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  base: '/personal-website/', // Matches your GitHub Pages URL path
 });
